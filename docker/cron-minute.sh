695a263c4c86003e1bb76572cf1281cdf0cdecd6
James Bithell <Jbithell@users.noreply.github.com>
2022-07-10 14:52:49 +0100

    Remove public sites (#1)

--- docker/cron-minute.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash -l
-#The -l is required to include environment variables in cron, not quite sure why but there we are
-/usr/local/bin/php /var/www/html/admin/api/article/cronArticle.php
--- docker/start.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-printenv | grep -v "no_proxy" >> /etc/environment #Required to include environment variables in cron
-
-cron -f &
-docker-php-entrypoint apache2-foreground
